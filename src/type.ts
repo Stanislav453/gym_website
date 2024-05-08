bae0c6b5870f2a0678f92b1091beabdb0b6ad28e
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 17:24:05 +0200

    edit type

--- src/type.ts
+++ src/type.ts
@@ -1,23 +1,31 @@
 
+
 export type sectionHeaderProps = {
-  hightText: string,
-  secondHightText?: string
-}
+  hightText: string;
+  secondHightText?: string;
+};
+
+export type modalServivesProps = {
+  serviceName: string;
+  setServiceName: React.Dispatch<React.SetStateAction<string>>;
+};
 
 export type imgModalProps = {
-  id:  number 
-  setModalState:  React.Dispatch<React.SetStateAction<{
-    isOpen: boolean;
-    id: number;
-}>>
-
-}
+  id: number;
+  setModalState: React.Dispatch<
+    React.SetStateAction<{
+      isOpen: boolean;
+      id: number;
+    }>
+  >;
+};
 
 export type imgProps = {
-    src: string
-  }
-  
+  src: string;
+};
+
 export type HeaderButtonProps = {
-    variant: string
-    children: React.ReactNode
-}
+  variant: string;
+  onClick: () => void;
+  children: React.ReactNode;
+};
