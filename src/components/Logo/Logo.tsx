c7e027538dd666e8a483ee032cd1a38ebf485000
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:44:46 +0200

    Create component for img whitch is logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import logo from "../../assets/adel-logo.svg";
+
+export const Logo = () => {
+  return (
+    <a href="#" className="w-24 md:w-16">
+      <img className="w-full h-full" src={logo} alt="logo-adela-zavrsanova" />
+    </a>
+  );
+};
