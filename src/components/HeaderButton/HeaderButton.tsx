14078a9f484121885f55d3d9530912ba8c9b4ac6
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 17:24:25 +0200

    add onClick props and method

--- src/components/HeaderButton/HeaderButton.tsx
+++ src/components/HeaderButton/HeaderButton.tsx
@@ -4,9 +4,10 @@ import { type HeaderButtonProps } from "../../type";
 const buttonBasicStyle =
   "flex-1 px-8 py-4 text-headerButton uppercase hover:shadow-HeadoeButtonHover transition-box-shadow";
 
-export const HeaderButton = ({ variant, children }: HeaderButtonProps) => {
+export const HeaderButton = ({ variant, children, onClick }: HeaderButtonProps) => {
   return (
     <button
+    onClick={onClick}
       className={
         variant === "transparent"
           ? `${buttonBasicStyle} border-4 border-black`
