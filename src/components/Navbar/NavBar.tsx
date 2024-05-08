a960f31b7123b4cde72a95e45c6c3e515498d5b9
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:45:04 +0200

    Create NavBar

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,20 +1,56 @@
-import { Link } from "react-router-dom"
-import { NavBarData } from "./NavBarData"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavBarData } from "./NavBarData";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 export const NavBar = () => {
+  const [navigationState, setNavigationState] = useState({id:0, isActive: false});
+  const {id, isActive } = navigationState
+
+  const navToggle = () => {
+    setNavigationState( (prev) => ({...prev, isActive: !isActive}) )
+  }
+
+  const setNavId = (newId: number) => { 
+    setNavigationState((prev) => ({ ...prev, id: newId }));
+  }
+
+
+    const actualizeNavState = (newId: number) => {
+      setNavId(newId),
+        setNavigationState((prev) => ({ ...prev, isActive: false }));
+    };
+
+
   return (
     <nav className="flex items-center">
-      <ul className="flex flex-row gap-4">
-        {
-          NavBarData.map( ( navItem, key ) => {{
+      <button className=" md:hidden bg-navHover p-4" onClick={navToggle}>
+        <RxHamburgerMenu className="text-hamburgerNavFontSize text-hamburgerMenuColor" />
+      </button>
+      <ul
+        className={`absolute z-50 ${
+          isActive ? "top-[83px] opacity-100" : "top-[-200px] opacity-0 md:opacity-100"
+        } md:top-0 left-0 md:relative w-full flex flex-col md:flex-row  md:gap-16 tracking-widest transition-top duration-300`}
+      >
+        {NavBarData.map((navItem, key) => {
+          {
             return (
-              <li>
-                <Link key={key} to={ `/${navItem}` } >{navItem}</Link>
+              <li key={key} className="flex">
+                <Link
+                  to={`/${navItem}`}
+                  onClick={ () => actualizeNavState(key)}
+                  className={`w-full text-white md:text-black text-center bg-navHover md:bg-white py-2 z-50 uppercase font-semibold md:hover:text-navHover transition-color ${
+                    key === id && "text-navHover"
+                  } `}
+                >
+                  {navItem}
+                </Link>
               </li>
-            )
-          }} )
-        }
+            );
+          }
+        })}
       </ul>
     </nav>
-  )
-}
+  );
+};
