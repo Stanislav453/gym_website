f1b35bfae31eee942c3a599b7a1991bccd314b1a
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:47:09 +0200

    Create voucher data for voucher page

---
+++ src/data/voucherData.tsx
@@ -0,0 +1,14 @@
+import voucher_front from "../assets/voucher/voucher_one.webp";
+import voucher_back from "../assets/voucher/voucher_two.webp";
+export const voucherData = {
+  voucherImg: [voucher_front, voucher_back],
+  voucherInfo: [
+    {
+      title: "Platnosť poukážky je 12 mesiacov od dátumu zakúpenia.",
+      info: "Viac info o službách nájdete v sekcii služby a spolupráca.",
+      contactTitle: "V prípade záujmu ma môžete kontaktovať:",
+      tel: "+421 903 358 214",
+      mail: "zavrsanovaadela@gmail.com",
+    },
+  ],
+};
