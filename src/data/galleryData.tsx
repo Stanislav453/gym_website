1f4a4d35b95c99d627fc616982089fd983aae2a7
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:45:45 +0200

    Create data for gallery

---
+++ src/data/galleryData.tsx
@@ -0,0 +1,35 @@
+import img_one from "../assets/gallery/foto-16.webp";
+import img_two from "../assets/gallery/foto-31.webp";
+import img_three from "../assets/gallery/foto-32.webp";
+import img_four from "../assets/gallery/foto-5.webp";
+import img_five from "../assets/gallery/foto-8.webp";
+import img_six from "../assets/gallery/foto-9.webp";
+import img_seven from "../assets/gallery/photo(2).webp";
+import img_eight from "../assets/gallery/photo.webp";
+
+export const galleryData = [
+  {
+    src: img_one,
+  },
+  {
+    src: img_two,
+  },
+  {
+    src: img_three,
+  },
+  {
+    src: img_four,
+  },
+  {
+    src: img_five,
+  },
+  {
+    src: img_six,
+  },
+  {
+    src: img_seven,
+  },
+  {
+    src: img_eight,
+  },
+];
