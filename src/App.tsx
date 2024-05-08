242de161b727de7b6b3f9228d4b95eb5f92f245f
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:41:36 +0200

    add React route / screate nav / add page transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
-import { NavBar } from "./components/Navbar/NavBar";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
-import { Header } from "./components/Header";
+import { Nav } from "./components/Nav";
+import { Footer } from "./components/Footer";
+import { AboutMy } from "./pages/AboutMy";
+import { References } from "./pages/References";
+import { Voucher } from "./pages/Voucher";
 
 function App() {
   const location = useLocation();
+  const [isPageLoaded, setIsPageLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsPageLoaded(!isPageLoaded);
+  }, []);
 
   return (
     <>
-      <Header />
+      <Nav />
       <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route index element={<Home />} />
-          <Route path="/domov" element={<Home />} />
-          <Route path="/galeria" element={<Gallery />} />
-        </Routes>
+        <motion.div
+          key={location.pathname}
+          initial={{ x: 10, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -10, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <Routes location={location} key={location.pathname}>
+            <Route index element={<Home />} />
+            <Route path="/domov" element={<Home />} />
+            <Route path="/galeria" element={<Gallery />} />
+            <Route path="/o mne" element={<AboutMy />} />
+            <Route path="/referencie" element={<References />} />
+            <Route path="/voucher" element={<Voucher />} />
+          </Routes>
+        </motion.div>
       </AnimatePresence>
+      <Footer />
     </>
   );
 }
