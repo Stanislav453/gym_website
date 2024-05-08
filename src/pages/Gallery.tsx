71722ddb731640f0c3cd5030c54f8f9ea2b7ffed
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:47:35 +0200

    Create gallery

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,9 +1,36 @@
-
+import { useState } from "react";
+import { Img } from "../components/Img/Img";
+import { ImgModal } from "../components/ImgModal/ImgModal";
+import { galleryData } from "../data/galleryData";
+import { SectionHeader } from "../components/SectionHeader/SectionHeader";
 
 const Gallery = () => {
-  return (
-    <div>gallery</div>
-  )
-}
+  const hightText = "fotogaleria";
+  const [imodalState, setModalState] = useState({ isOpen: false, id: 0 });
+  const { isOpen, id } = imodalState;
 
-export default Gallery
+  return (
+    <section>
+      <header>
+        <SectionHeader hightText={hightText} />
+      </header>
+      <main className="flex flex-wrap gap-5 justify-center w-full h-full pt-16">
+        {galleryData.map((onePhoto, index) => {
+          return (
+            <div
+              key={index}
+              className=" flex-initial w-72 h-80	"
+              onClick={() => setModalState({ isOpen: true, id: index })}
+            >
+              {" "}
+              <Img src={onePhoto.src} />{" "}
+            </div>
+          );
+        })}
+      </main>
+      {isOpen && <ImgModal id={id} setModalState={setModalState} />}
+    </section>
+  );
+};
+
+export default Gallery;
