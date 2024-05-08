2cbd0c05aef6fb445729b43fb3ef536a3061cbf0
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-08 10:46:43 +0200

    Create references data for references page.

---
+++ src/data/referencesData.tsx
@@ -0,0 +1,34 @@
+export const referencesData = [
+  {
+    title: " Skvelá spolupráca, z ktorej si veľa odnášam",
+    text: ",,Na Adel som sa obrátila hlavne preto, aby som si zlepšila techniku cvikov a dala tréningom štruktúru. Pri plánovaní stretnutí bola vždy ústretová a maximálne flexibilná. Každý tréning so mnou prebrala a vysvetlila jednotlivé cviky. Pri cvičení dávala pozor na správnu techniku a prispôsobovala cviky mojim možnostiam. Pomohla mi s tým, si uvedomiť, že pre každého je tá správna technika iná. Rovnako mi pomohla aj nabrať trochu sebavedomia v prevedení cvikov. Na tréningu bola vždy super atmosféra. Adel je veľmi priateľská a pri tom profesionálna. Aj po skončení spoločných tréningov sa stále zaujíma ako postupujem s jej tréningovým plánom, aký progress zaznamenávam, alebo či nie sú nejaké problémy. Skvelá spolupráca, z ktorej si veľa odnášam.“",
+    name: "Juliána B.",
+  },
+  {
+    title: "Páči sa mi aj to, že to nie je len o trénerke a klientke.",
+    text: ",,Podľa mňa som si lepšiu trénerku nemohla vybrať :-) si milá, priateľská, úprimná a hlavne tí záleží v prvom rade na zdraví :-) ja som veľmi spokojná a som rada, že som ťa oslovila ❤ páči sa mi aj to, že to nie je len o trénerke a klientke ale že máme k sebe taký viac kamarátsky postoj, že sa aj na tréningoch zasmejeme a pod. Nemám čo vytknúť :-)“",
+    name: "Laura B.",
+  },
+  {
+    title:
+      ",,Tréningy sú naozaj veľmi dobre prispôsobené tomu, čo naozaj potrebujem. Takisto sa s ňou dá vždy pekne dohodnúť kedy bude ďalší tréning. V našej spolupráci sme síce len na začiatku, ale už aj pri týchto tréningoch som sa veľa naučila.Naozaj môžem len odporučiť.",
+    text: ",,Tréningy sú naozaj veľmi dobre prispôsobené tomu, čo naozaj potrebujem. Takisto sa s ňou dá vždy pekne dohodnúť kedy bude ďalší tréning. V našej spolupráci sme síce len na začiatku, ale už aj pri týchto tréningoch som sa veľa naučila.Naozaj môžem len odporučiť.“",
+    name: "Daniela K.",
+  },
+  {
+    title: "Tréningy sa nesú v pohodovej atmosfére.",
+    text: ",,Všetko ukáže a do ničoho nenúti. Priateľská a milá osoba, ktorá robí svoju prácu profesionálne.“",
+    name: "Nikol M.",
+  },
+  {
+    title:
+      "S Adel spolupracujem už rok, za ktorý som sa veľa naučila, posunula a zdokonalila",
+    text: ",,Adel mi ponúkla mnoho užitočných informácií aby som aj ja sama pochopila ako všetko funguje. Podrobne mi vysvetlila čo nás bude čakať a podala mi inštrukcie ktorých som sa držala. Musím povedať že som mala zo začiatku obavy no vďaka jej profesionalite išlo všetko hladko. Počas spolupráce sme každým tréningom zdokonaľovali techniku pri jednotlivých cvikoch, aby som následne dokázala samostatne cvičiť bez akýchkoľvek obáv. Adel tu pre mňa bola keď som mala nejasnosti a pohotovo mi všetko vždy vysvetlila. Naša spolupráca priniesla super výsledky a veľmi sa teším z jej pokračovania v podobe súťažnej prípravy. V neposlednom rade jej chcem poďakovať za každý náš progres, osobne tréningy, tréningové plány, jej čas a profesionalitu, ktorú zriedka nájdete hlavne pri mladých tréneroch.“",
+    name: "Júlia R.",
+  },
+  {
+    title: "Po malých krôčikoch sa vďaka nej posúvam vpred.",
+    text: ",,Adel mi fakt veľa pomohla, čo sa týka techniky pri cvičení alebo v strave. Ukázala mi spôsob ako sa nemám v ničom obmedzovať, či je to zdravé alebo nezdravé. Páči sa mi jej komunikácia, či už takto online alebo osobne. Vždy poradí ohľadom cvičenia, stravy a som veľmi rada, že som si ju vybrala.“",
+    name: "Juliána B.",
+  },
+];
