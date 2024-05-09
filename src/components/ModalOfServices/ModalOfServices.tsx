48eefed8e3e88bc352088ccf050a83f5916f44ef
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-05-09 15:47:23 +0200

    add state serviceArticleData, add modalSrviceData.

--- src/components/ModalOfServices/ModalOfServices.tsx
+++ src/components/ModalOfServices/ModalOfServices.tsx
@@ -8,35 +8,32 @@ export const ModalOfServices = ({
   serviceName,
   setServiceName,
 }: modalServivesProps) => {
+  const [modalSrviceData, setModalServiceData] = useState(litvaFitnessData);
+  const [ serviceArticleData, setServiceArticleData ] = useState([])
 
-    const [modalSrviceData, setModalServiceData] = useState(litvaFitnessData);
+  const setServicesData = (name: string) => {
+    switch (name) {
+      case "litva fitness": {
+        setModalServiceData(litvaFitnessData);
+        break;
+      }
+      case "zØna": {
+        setModalServiceData(zonaData);
+        break;
+      }
+      default:
+        console.log("data do not set");
+    }
+  };
+
+  useEffect(() => {
+    setServicesData(serviceName);
+  }, [modalSrviceData]);
 
 
-      const setServicesData = (name: string) => {
-        switch (name) {
-          case "litva fitness": {
-            setModalServiceData(litvaFitnessData);
-            break;
-          }
-          case "zØna": {
-            setModalServiceData(zonaData);
-            break;
-          }
-          default:
-            console.log("data do not set");
-        }
-      };
-
-      useEffect(() => {
-        setServicesData(serviceName);
-      }, [modalSrviceData]);
-
-
-     console.log(modalSrviceData);
-     
 
   return (
-    <section className="fixed top-0 z-50 w-full h-full bg-modalBackgroundColor">
+    <section className="flex fixed top-0 z-50 w-full h-full bg-modalBackgroundColor">
       <button className="absolute top-2 right-2 ">
         <FaCircleXmark
           className="text-modalIcon text-white cursor-pointer transition-transform hover:scale-110"
@@ -48,9 +45,40 @@ export const ModalOfServices = ({
           {modalSrviceData.hightText}
         </h2>
         <h3 className="uppercase text-center">{modalSrviceData.title}</h3>
-        <ul>
-            
+        <ul className="flex flex-col gap-1 pt-5">
+          {modalSrviceData.servicesData.map((oneButton, index) => {
+            const { serviceHight, servicesCategory } = oneButton;
+            return (
+              <li key={index} className="mx-1">
+                <button className="w-full bg-mainColor py-3 uppercase">
+                  {serviceHight}
+                </button>
+                <ul>
+                  {servicesCategory.map((oneCategory, index) => {
+                    const { categoryTitle } = oneCategory;
+                    return (
+                      <li key={index}>
+                        <button
+                          className="w-full bg-subMainColor py-3 uppercase mt-[.1rem]"
+                          onClick={ () => { setArticleData(index)  } }
+                        >
+                          {categoryTitle}
+                        </button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
         </ul>
+      </article>
+      <article className="w-2/4 h-full  bg-subMainColor">
+        {/* {
+          articleData.map( (item, key) => {
+            return item.categoryTitle;
+          } )
+        } */}
       </article>
     </section>
   );
